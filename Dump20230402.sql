87440b7d96ba65d560230ad685886440d243871a
Rafael Caal <rafael.caal@gmail.com>
2023-04-02 17:41:55 -0600

    cambios a db

--- Dump20230402.sql
+++ Dump20230402.sql
@@ -508,6 +508,28 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
+-- Table structure for table `tablaprueba`
+--
+
+DROP TABLE IF EXISTS `tablaprueba`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tablaprueba` (
+  `id` int NOT NULL,
+  `dat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tablaprueba`
+--
+
+LOCK TABLES `tablaprueba` WRITE;
+/*!40000 ALTER TABLE `tablaprueba` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tablaprueba` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping events for database 'supermercado'
 --
 /*!50106 SET @save_time_zone= @@TIME_ZONE */ ;
@@ -1094,4 +1116,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-02 17:20:04
+-- Dump completed on 2023-04-02 17:41:07
